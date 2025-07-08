3706675a07265e2132140c1841563c656d41224f
Claudia <cludama83@gmail.com>
2025-07-07 23:56:52 -0400

    Updated message components, completed MessageForm, PetMessages, initiated app.jsx and added messages code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+//import NavBar from
+import Inbox from "./components/messages/Inbox";
+import MessageForm from "./components/messages/MessageForm";
+import PetMessages from "./components/messages/PetMessages";
+import "./App.css";
 
 function App() {
+  const [token, setToken] = useState(
+    () => localStorage.getItem("authToken") || ""
+  );
+
   return (
-    <>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Inbox token={token} />} />
+        <Route
+          path="/message-form"
+          element={
+            <MessageForm
+              token={token}
+              receiverId={2}
+              petId={1}
+              onMessageSent={(msg) => console.log("Message sent:", msg)}
+            />
+          }
+        />
+        <Route
+          path="/pet/:id/messages"
+          element={<PetMessages token={token} petId={1} />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
